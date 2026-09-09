9b3fd4f8f9ff02a64be3d879519711de48c43bca
outofthelotmedia <outofthelot.media@gmail.com>
2026-09-08 20:37:34 -0700

    09/08/26
    some fixes, should be last one

--- js/main.js
+++ js/main.js
@@ -58,7 +58,7 @@ if (scrollHint) {
 //
 // Once your proxy is deployed, paste its URL below. Nothing secret ever
 // lives in this file or in GitHub.
-const PROXY_URL = 'https://ootl-youtube-proxy.scottkindelbergerlikescars.workers.dev/latest-video';
+const PROXY_URL = 'https://YOUR-WORKER-SUBDOMAIN.workers.dev/latest-video';
 const MIN_SECS  = 4 * 60;
 
 async function loadLatestEpisode() {
